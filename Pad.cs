474acb354e41966a4515ab93254f8ad09538fb98
alexhendrik <45019513+alexhendrik@users.noreply.github.com>
2021-12-01 22:06:49 -0500

    Add project files.

---
+++ Pad.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneTimePad
+{
+    internal class Pad : IDisposable
+    {
+        /// <summary>
+        /// Byte array storage for the single use key.
+        /// </summary>
+        private byte[] TempKey { get; set; }
+
+        /// <summary>
+        /// Initializes a pad instance and populates the key using a pseudo-random function.
+        /// </summary>
+        /// <param name="keyLengthBytes">Desired key length in bytes.</param>
+        public Pad(int keyLengthBytes)
+        {
+            TempKey = new byte[keyLengthBytes];
+
+            using (var gen = RandomNumberGenerator.Create()) // Dispose of the RNG as soon as it populates the key
+                gen.GetBytes(TempKey);
+
+            Console.WriteLine($"Created a new OTP instance with key:\t{System.Convert.ToHexString(TempKey)}");
+        }
+
+        /// <summary>
+        /// Performs the XOR conversion with the given input and the stored single use key.
+        /// </summary>
+        /// <param name="input">Either the plaintext or the ciphertext array.</param>
+        /// <returns>Result of the XOR operation.</returns>
+        public byte[] Convert(byte[] input)
+        {
+            var result = new byte[input.Length];
+
+            if (input.Length != TempKey.Length)
+                throw new ArgumentException("The input length does not match the length of the key!");
+
+            for(int b = 0; b < input.Length; b++)
+                result[b] = (byte)(input[b] ^ TempKey[b]);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Provisional method for decrypting with a key from an external source, currently unused.
+        /// </summary>
+        /// <param name="ciphertext">The ciphertext array.</param>
+        /// <param name="newKey">Externally sourced key.</param>
+        /// <returns>Result of the XOR operation.</returns>
+        public byte[] ConvertWithNewKey(byte[] ciphertext, byte[] newKey)
+        {
+            TempKey = newKey;
+
+            return Convert(ciphertext);
+        }
+
+        /// <summary>
+        /// Clean the memory locations of the single use key and run the garbage collector.
+        /// </summary>
+        public void Dispose()
+        {
+            for(int i = 0; i < TempKey.Length; i++)
+                TempKey[i] = 0;
+
+            GC.Collect();
+            GC.SuppressFinalize(this);
+
+            Console.WriteLine("OTP instance has been sanitized and disposed.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace OneTimePad
+{
+    public class Program
+    {
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            ProgramLoop();
+        }
+
+        /// <summary>
+        /// Primary program execution loop.
+        /// </summary>
+        private static void ProgramLoop()
+        {
+            Console.WriteLine("Welcome to TSOTP: Totally Secure One Time Pad\nBy Alex Hendrik for CS 538");
+
+            while (true)
+            {
+                var plaintextBytes = Encoding.UTF8.GetBytes(GatherUserInput());
+
+                Console.WriteLine("\n-----\n");
+
+                using (var pad = new Pad(plaintextBytes.Length))
+                {
+                    var ciphertext = pad.Convert(plaintextBytes);
+
+                    Console.WriteLine($"Hex Plaintext:\t\t\t\t{Convert.ToHexString(plaintextBytes)}");
+                    Console.WriteLine($"Ciphertext:\t\t\t\t{Convert.ToHexString(ciphertext)}");
+                    Console.WriteLine($"Test Decryption:\t\t\t{Encoding.UTF8.GetString(pad.Convert(ciphertext))}");
+                }
+
+                Console.WriteLine("\n#####\n");
+            }
+        }
+
+        /// <summary>
+        /// Repeatedly attempt to gather plaintext, verify, and sanitize it.
+        /// </summary>
+        /// <returns>Sanitized plaintext.</returns>
+        private static string GatherUserInput()
+        {
+            Console.WriteLine("\nPlease enter the desired plaintext or use Ctrl + C to exit:");
+            var unsafeIn = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(unsafeIn) || unsafeIn.Any(c => !char.IsLetterOrDigit(c)))
+            {
+                Console.WriteLine("Invalid plaintext provided, please make sure you avoid special characters!");
+                unsafeIn = GatherUserInput();
+            }
+
+            return unsafeIn.Trim().ToLower();
+        }
+    }
+}
